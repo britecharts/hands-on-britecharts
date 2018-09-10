a13ebca45800c0216ed756a1c2d24b4b3c08aa71
Daler Asrorov <asrorids@gmail.com>
2018-09-10 00:57:58 -0700

    Switch to webpack dev server on start

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,13 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    contentBase: './'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: "/dist/",
     filename: 'bundle.js'
   }
 };
